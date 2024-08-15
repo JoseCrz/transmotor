5568015c920791826be0d1710cf6916247055362
Jose Cuevas <josecueram@gmail.com>
2024-08-15 08:18:03 -0600

    Install `@radix-ui/react-accordion`

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,30 @@ const config: Config = {
       fontFamily: {
         sans: ["var(--manrope)"],
       },
+      keyframes: {
+        "accordion-open": {
+          from: { width: "0" },
+          to: { width: "var(--radix-accordion-content-width)" },
+        },
+        "accordion-close": {
+          from: { width: "var(--radix-accordion-content-width)" },
+          to: { width: "0" },
+        },
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-open": "accordion-open 0.2s ease-out",
+        "accordion-close": "accordion-close 0.2s ease-out",
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],

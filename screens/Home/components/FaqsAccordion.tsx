3b21f41a43c5ac148ce032c04012f253aacecec7
Jose Cuevas <josecueram@gmail.com>
2024-08-16 21:13:40 -0600

    Update `<FaqsAccordion />` for mobile

--- screens/Home/components/FaqsAccordion.tsx
+++ screens/Home/components/FaqsAccordion.tsx
@@ -10,7 +10,7 @@ export function FaqsAccordion({
     <Accordion.Root
       type="multiple"
       defaultValue={defaultValue}
-      className="mx-auto rounded-lg bg-white shadow-lg xl:max-w-[768px] xl:p-6"
+      className="mx-auto rounded-lg bg-white p-6 shadow-lg xl:max-w-[768px]"
     >
       {children}
     </Accordion.Root>
@@ -24,7 +24,7 @@ export function FaqsAccordionItem({
   return (
     <Accordion.Item
       value={value}
-      className="border-b border-dashed border-[#AFBDCB] last-of-type:border-none"
+      className="border-b border-dashed border-[#AFBDCB] py-5 first-of-type:pt-0 last-of-type:border-none last-of-type:pb-0"
     >
       {children}
     </Accordion.Item>
@@ -33,7 +33,7 @@ export function FaqsAccordionItem({
 
 export function FaqsAccordionTrigger({ children }: PropsWithChildren) {
   return (
-    <Accordion.Header className="flex w-full xl:py-5">
+    <Accordion.Header className="flex w-full">
       <Accordion.Trigger className="group flex w-full items-center justify-between text-xl">
         {children}
         <ChevronDown
@@ -48,7 +48,7 @@ export function FaqsAccordionTrigger({ children }: PropsWithChildren) {
 export function FaqsAccordionContent({ children }: PropsWithChildren) {
   return (
     <Accordion.Content className="overflow-hidden transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down">
-      <div className="pb-6">{children}</div>
+      <div className="py-6">{children}</div>
     </Accordion.Content>
   );
 }

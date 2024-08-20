c032d697e2294be229be6caf6792aa53ae08e664
Jose Cuevas <josecueram@gmail.com>
2024-08-20 11:47:53 -0600

    Update `<Layout />` architecture

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -27,119 +27,27 @@ import {
   NavigationMenuTrigger,
 } from "../NavigationMenu";
 
-import logo from "./images/logo.svg";
+import logoWhite from "./images/logo.svg";
 import logoBlack from "./images/logo-black.svg";
 import { cn } from "@/utils";
 
 type LayoutProps = {
-  children: ReactNode;
+  headerVariant?: HeaderVariant;
   footerClassName?: string;
+  children: ReactNode;
 };
 
-export function Layout({ footerClassName, children }: LayoutProps) {
+export function Layout({
+  headerVariant = "default",
+  footerClassName,
+  children,
+}: LayoutProps) {
   return (
     <>
       <header className="fixed top-0 z-10 w-full py-4">
         <Container>
-          <div className="flex justify-between lg:hidden">
-            <Image src={logo} alt="Transmotor Logo" height={27} priority />
-
-            <Dialog.Root>
-              <Dialog.Trigger className="flex rounded bg-white p-1 shadow">
-                <Menu />
-              </Dialog.Trigger>
-              <Dialog.Portal>
-                <Dialog.Overlay />
-                <Dialog.Content
-                  className={`fixed left-[50%] top-[50%] z-50 h-dvh w-full translate-x-[-50%] translate-y-[-50%] bg-white px-6 py-4 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg ${manrope.className} font-sans`}
-                >
-                  <Dialog.Title className="sr-only">Menu</Dialog.Title>
-                  <div className="flex justify-end">
-                    <Dialog.Close className="rounded bg-black p-1 text-white">
-                      <X />
-                    </Dialog.Close>
-                  </div>
-                  <div className="mt-24">
-                    <nav>
-                      <ul className="space-y-8">
-                        <MobileMenuItem href="/PENDING" Icon={PlugZap}>
-                          Instalaciones de baja, media y alta tensión
-                        </MobileMenuItem>
-                        <MobileMenuItem href="/" Icon={Dam}>
-                          Instalaciones hidráulicas
-                        </MobileMenuItem>
-                        <MobileMenuItem href="/" Icon={FireExtinguisher}>
-                          Instalaciones contra incendio
-                        </MobileMenuItem>
-                        <MobileMenuItem href="/" Icon={FileStack}>
-                          Trámites CFE, SENER, UVIE
-                        </MobileMenuItem>
-                        <MobileMenuItem href="/" Icon={CarFront}>
-                          Servicio de grua
-                        </MobileMenuItem>
-                      </ul>
-                    </nav>
-                    <div className="mt-52 flex justify-center">
-                      <ButtonLink variant="electric" href="PENDING">
-                        Contáctanos
-                      </ButtonLink>
-                    </div>
-                  </div>
-                </Dialog.Content>
-              </Dialog.Portal>
-            </Dialog.Root>
-          </div>
-          <div className="hidden lg:flex lg:items-center">
-            <div className="flex flex-1 justify-start">
-              <Image src={logo} alt="Transmotor Logo" height={27} priority />
-            </div>
-            <NavigationMenu className="rounded-md bg-white px-6 py-2 shadow-lg">
-              <NavigationMenuList className="gap-6">
-                <NavigationMenuItem>
-                  <NavigationMenuLinkTrigger href="/">
-                    Home
-                  </NavigationMenuLinkTrigger>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                  <NavigationMenuTrigger>Servicios</NavigationMenuTrigger>
-                  <NavigationMenuContent className="min-w-[374px] border bg-white px-4 py-6">
-                    <ul className="space-y-4">
-                      <ListItemLink href="/" Icon={PlugZap}>
-                        Instalaciones de baja, media y alta tensión
-                      </ListItemLink>
-                      <ListItemLink href="/" Icon={Dam}>
-                        Instalaciones hidráulicas
-                      </ListItemLink>
-                      <ListItemLink href="/" Icon={FireExtinguisher}>
-                        Instalaciones contra incendio
-                      </ListItemLink>
-                      <ListItemLink href="/" Icon={FileStack}>
-                        Trámites CFE, SENER, UVIE
-                      </ListItemLink>
-                      <ListItemLink href="/" Icon={CarFront}>
-                        Servicio de grua
-                      </ListItemLink>
-                    </ul>
-                  </NavigationMenuContent>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                  <NavigationMenuLinkTrigger href="/proyectos">
-                    Proyectos
-                  </NavigationMenuLinkTrigger>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                  <NavigationMenuLinkTrigger href="/nosotros">
-                    Acerca de nosotros
-                  </NavigationMenuLinkTrigger>
-                </NavigationMenuItem>
-              </NavigationMenuList>
-            </NavigationMenu>
-            <div className="flex flex-1 justify-end">
-              <ButtonLink variant="crystal" href="/">
-                Contacto
-              </ButtonLink>
-            </div>
-          </div>
+          <MobileHeader variant={headerVariant} />
+          <DesktopHeader variant={headerVariant} />
         </Container>
       </header>
       <main>{children}</main>
@@ -204,7 +112,138 @@ export function Layout({ footerClassName, children }: LayoutProps) {
   );
 }
 
+type HeaderVariant = "default" | "transparent";
+
 // ================ Local Components ================
+
+function MobileHeader({ variant = "default" }: { variant?: HeaderVariant }) {
+  const logo = variant === "default" ? logoBlack : logoWhite;
+
+  return (
+    <div className="flex justify-between lg:hidden">
+      <Link href="/">
+        <Image src={logo} alt="Transmotor Logo" height={27} priority />
+      </Link>
+
+      <Dialog.Root>
+        <Dialog.Trigger
+          className={cn(
+            "flex rounded p-1 shadow",
+            variant === "default" ? "bg-[#E5E5E5]" : "bg-white",
+          )}
+        >
+          <Menu />
+        </Dialog.Trigger>
+        <Dialog.Portal>
+          <Dialog.Overlay />
+          <Dialog.Content
+            className={`fixed left-[50%] top-[50%] z-50 h-dvh w-full translate-x-[-50%] translate-y-[-50%] bg-white px-6 py-4 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg ${manrope.className} font-sans`}
+          >
+            <Dialog.Title className="sr-only">Menu</Dialog.Title>
+            <div className="flex justify-end">
+              <Dialog.Close className="rounded bg-black p-1 text-white">
+                <X />
+              </Dialog.Close>
+            </div>
+            <div className="mt-24">
+              <nav>
+                <ul className="space-y-8">
+                  <MobileMenuItem href="/PENDING" Icon={PlugZap}>
+                    Instalaciones de baja, media y alta tensión
+                  </MobileMenuItem>
+                  <MobileMenuItem href="/" Icon={Dam}>
+                    Instalaciones hidráulicas
+                  </MobileMenuItem>
+                  <MobileMenuItem href="/" Icon={FireExtinguisher}>
+                    Instalaciones contra incendio
+                  </MobileMenuItem>
+                  <MobileMenuItem href="/" Icon={FileStack}>
+                    Trámites CFE, SENER, UVIE
+                  </MobileMenuItem>
+                  <MobileMenuItem href="/" Icon={CarFront}>
+                    Servicio de grua
+                  </MobileMenuItem>
+                </ul>
+              </nav>
+              <div className="mt-52 flex justify-center">
+                <ButtonLink variant="electric" href="PENDING">
+                  Contáctanos
+                </ButtonLink>
+              </div>
+            </div>
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
+    </div>
+  );
+}
+
+function DesktopHeader({ variant = "default" }: { variant?: HeaderVariant }) {
+  const logo = variant === "default" ? logoBlack : logoWhite;
+
+  return (
+    <div className="hidden lg:flex lg:items-center">
+      <div className="flex flex-1 justify-start">
+        <Link href="/">
+          <Image src={logo} alt="Transmotor Logo" height={27} priority />
+        </Link>
+      </div>
+      <NavigationMenu
+        className={cn(
+          "rounded-md px-6 py-2",
+          variant === "default" ? "bg-[#F0F0F0] shadow" : "bg-white shadow-lg",
+        )}
+      >
+        <NavigationMenuList className="gap-6">
+          <NavigationMenuItem>
+            <NavigationMenuLinkTrigger href="/">Home</NavigationMenuLinkTrigger>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Servicios</NavigationMenuTrigger>
+            <NavigationMenuContent className="min-w-[374px] border bg-white px-4 py-6">
+              <ul className="space-y-4">
+                <ListItemLink href="/" Icon={PlugZap}>
+                  Instalaciones de baja, media y alta tensión
+                </ListItemLink>
+                <ListItemLink href="/" Icon={Dam}>
+                  Instalaciones hidráulicas
+                </ListItemLink>
+                <ListItemLink href="/" Icon={FireExtinguisher}>
+                  Instalaciones contra incendio
+                </ListItemLink>
+                <ListItemLink href="/" Icon={FileStack}>
+                  Trámites CFE, SENER, UVIE
+                </ListItemLink>
+                <ListItemLink href="/" Icon={CarFront}>
+                  Servicio de grua
+                </ListItemLink>
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLinkTrigger href="/proyectos">
+              Proyectos
+            </NavigationMenuLinkTrigger>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLinkTrigger href="/nosotros">
+              Acerca de nosotros
+            </NavigationMenuLinkTrigger>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+      <div className="flex flex-1 justify-end">
+        <ButtonLink
+          variant={variant === "default" ? "ghost" : "crystal"}
+          href="/"
+          className="shadow"
+        >
+          Contacto
+        </ButtonLink>
+      </div>
+    </div>
+  );
+}
 
 type MobileMenuItemProps = {
   href: string;

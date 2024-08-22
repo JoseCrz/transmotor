ca721d9a495b2e5dda1d2533010298e41ada57d3
Jose Cuevas <josecueram@gmail.com>
2024-08-22 12:29:59 -0600

    Update `<Container />` styles

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,5 +1,5 @@
 import type { PropsWithChildren } from "react";
 
 export function Container({ children }: PropsWithChildren) {
-  return <div className="px-6 md:px-16">{children}</div>;
+  return <div className="container mx-auto px-6">{children}</div>;
 }

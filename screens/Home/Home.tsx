15ca801ef66e37df55e012cf3dde08f96a09ef70
Jose Cuevas <josecueram@gmail.com>
2024-08-22 10:59:46 -0600

    Create `<ContactSection />` section

--- screens/Home/Home.tsx
+++ screens/Home/Home.tsx
@@ -1,6 +1,5 @@
 import type { PropsWithChildren, ReactNode } from "react";
 import Image, { ImageProps } from "next/image";
-import { Mail, Phone, MapPinned } from "lucide-react";
 
 import { cn } from "@/utils";
 
@@ -15,7 +14,7 @@ import {
   ServiceArticle,
 } from "@/components";
 
-import { CTASection } from "@/sections";
+import { CTASection, ContactSection } from "@/sections";
 
 import {
   ProjectsAccordion,
@@ -45,7 +44,6 @@ import accordion1 from "./images/accordion-1.jpeg";
 import gridLogo from "./images/grid-logo.svg";
 
 import combBackground from "./images/comb-background.svg";
-import contactImage from "./images/contact.jpeg";
 
 export function Home() {
   return (
@@ -447,50 +445,7 @@ export function Home() {
         </Container>
       </section>
       <CTASection className="bg-[#F6F6F6]" />
-      <section className="py-28">
-        <Container>
-          <h2 className="text-4xl md:text-6xl">Contacto</h2>
-          <p className="mt-1 md:mt-6">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
-          </p>
-          <div className="mt-8 flex flex-col justify-between gap-20 xl:mt-32 xl:flex-row">
-            <div className="space-y-10">
-              <div>
-                <Mail />
-                <a
-                  href="mailto:hello@transmotor.com.mx"
-                  className="mt-4 block text-sm md:text-base"
-                >
-                  hello@transmotor.com.mx
-                </a>
-              </div>
-              <div>
-                <Phone />
-                <a
-                  href="tel:+1555000000"
-                  className="mt-4 block text-sm md:text-base"
-                >
-                  +1 (555) 000-000
-                </a>
-              </div>
-              <div>
-                <MapPinned />
-                <a href="PENDING" className="mt-4 block text-sm md:text-base">
-                  Centro Xalapa, Veracruz
-                </a>
-              </div>
-            </div>
-            <div>
-              <Image
-                src={contactImage}
-                alt="PENDING"
-                placeholder="blur"
-                className="rounded-xl object-cover xl:h-[516px] xl:w-auto"
-              />
-            </div>
-          </div>
-        </Container>
-      </section>
+      <ContactSection />
     </Layout>
   );
 }
--- sections/index.ts
+++ sections/index.ts
@@ -1,1 +1,2 @@
+export * from "./ContactSection/ContactSection";
 export * from "./CTASection";
